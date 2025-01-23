9f4c94e4cc9b635e4ffc2a8c7aa5eec2b269cf3a
andrefilipe1310 <andrefilipef1310@gmail.com>
2025-01-23 19:48:42 -0300

    estrutura inicial do projeto

---
+++ backend/src/main/java/com/kopi/bliss/BlissApplication.java
@@ -0,0 +1,13 @@
+package com.kopi.bliss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlissApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kopi/bliss/BlissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopi.bliss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
