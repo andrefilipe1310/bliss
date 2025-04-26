f7ab9ffd937d5a70496faf235728f43b90b7a385
andrefilipe1310 <andrefilipef1310@gmail.com>
2025-04-25 22:20:16 -0300

    ajustes no css da main

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -11,9 +11,14 @@ const routes: Routes = [
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
   {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
-  },  {
+  },
+  {
     path: 'wedding-information',
     loadChildren: () => import('./pages/wedding-information/wedding-information.module').then( m => m.WeddingInformationPageModule)
   },
